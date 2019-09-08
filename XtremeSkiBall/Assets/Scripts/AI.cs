c7e2ef7affe3f0f01c684732059d7ee1ea40361a
John Strange <john.strange.nz@gmail.com>
2019-09-09 11:29:04 +1200

    AI Gets standard assests

---
+++ XtremeSkiBall/Assets/Scripts/AI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    public GameObject Destination;
+    NavMeshAgent Agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Agent.SetDestination(Destination.transform.position);
+    }
+}
