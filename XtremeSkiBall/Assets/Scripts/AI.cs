abdc471ac51e7ad9aa16311d5da63901369cc12b
John Strange <john.strange.nz@gmail.com>
2019-09-09 16:48:29 +1200

    AI has States and basic movement and stuff

--- XtremeSkiBall/Assets/Scripts/AI.cs
+++ XtremeSkiBall/Assets/Scripts/AI.cs
@@ -1,21 +1,75 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class AI : MonoBehaviour
 {
+    public Vector3 LastSeenPosition;
+    public float FiringRange { get; private set; }
+    public float PickupDistance = 0.1f;
+    public List<Transform> WeaponPositions;
     public GameObject Destination;
-    NavMeshAgent Agent;
+    public GameObject[] Players;
+    public Collider[] playerColls;
+    public LayerMask obstacleMask;
+    public Camera AiCam { get; private set; }
+    public Plane[] planes;
+    public Transform Target { get; private set; }
+    public int currentTarget;
+
+    public NavMeshAgent Agent;
+
+    public StateMachine StateMachine => GetComponent<StateMachine>();
+    public bool hasWeapon {get; private set; }
+
+    private void Awake()
+    {
+        FiringRange = 1.0f;
+        Agent = GetComponent<NavMeshAgent>();
+        AiCam = GetComponentInChildren<Camera>();
+        Players = FindObjectOfType<CharacterInfo>().Players;
+        playerColls = FindObjectOfType<CharacterInfo>().PlayersCoil;
+        InitializeStateMachine();
+        hasWeapon = true;
+    }
+
+    private void InitializeStateMachine()
+    {
+        var states = new Dictionary<Type, BaseState>()
+        {
+            {typeof(WanderState), new WanderState(this) },
+            {typeof(ChaseState), new ChaseState(this) },
+            {typeof(AttackState), new AttackState(this) }
+        };
+
+        GetComponent<StateMachine>().SetStates(states);
+    }
+
+    public void SetTarget(Transform _Target, int _CurrentPlayer)
+    {
+        Target = _Target;
+        currentTarget = _CurrentPlayer;
+    }
     // Start is called before the first frame update
     void Start()
     {
         Agent = GetComponent<NavMeshAgent>();
     }
+    public void FireWeapon()
+    {
+        //Firing Script stuff goes here
+        hasWeapon = false;
+    }
 
     // Update is called once per frame
     void Update()
+    {       
+    }
+
+    private void OnTriggerEnter(Collider other)
     {
-        Agent.SetDestination(Destination.transform.position);
+        //Pickup Script goes here
     }
 }
