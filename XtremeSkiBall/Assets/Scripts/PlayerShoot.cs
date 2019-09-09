f8feb1d1223f48c30477b68239b5010bb0f8a03d
profawesum <profawesum@gmail.com>
2019-09-09 12:09:03 +1200

    Updates to weapon throwing

---
+++ XtremeSkiBall/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+
+    public bool hasWeapon = false;
+    public GameObject weapon;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+            if (hasWeapon) {
+                Instantiate(weapon, transform.position, transform.rotation);
+                hasWeapon = false;
+            }
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "pickupWeapon") {
+            hasWeapon = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+
+
+}
---
+++ XtremeSkiBall/Assets/Scripts/weaponVelocity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponVelocity : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float speed;
+
+    public float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(transform.forward * speed);
+    }
+
+    private void Update()
+    {
+        timer+= Time.deltaTime;
+        if (timer >= 15) {
+            Destroy(this.gameObject);
+        }
+    }
+}
