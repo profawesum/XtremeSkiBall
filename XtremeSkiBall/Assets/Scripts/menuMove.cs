d155a003f0498485ff6d0a427eac3cf4c2e1670c
prof <profawesum@gmail.com>
2019-09-11 19:58:41 +1200

    menu code sorted
    splash screen moves into menu which then moves into the game when play is clicked and quits when quit is clicked

---
+++ XtremeSkiBall/Assets/Scripts/menuMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuMove : MonoBehaviour
+{
+
+
+    public void onPlayClicked() {
+        Application.LoadLevel(2);
+    }
+
+    public void onQuitClick() {
+        Application.Quit();
+    }
+
+}
---
+++ XtremeSkiBall/Assets/Scripts/splashMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class splashMove : MonoBehaviour
+{
+
+
+    public GameObject videoplayer; // GameObject having the attached video
+    public VideoPlayer videofile; // VideoPLayer component 
+    public bool setAwake = false;
+
+    private void Awake()
+    {
+        setAwake = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (videofile.isPaused) {
+            Application.LoadLevel(1);
+        }
+    }
+}
