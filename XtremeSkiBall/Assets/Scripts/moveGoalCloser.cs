732a852495057ae0508f0300b9023ab6e96b551b
prof <profawesum@gmail.com>
2019-10-11 14:28:17 +1300

    Got the goal to move and limit the amount that it can move

--- XtremeSkiBall/Assets/Scripts/moveGoalCloser.cs
+++ XtremeSkiBall/Assets/Scripts/moveGoalCloser.cs
@@ -10,6 +10,29 @@ public class moveGoalCloser : MonoBehaviour
     public float posToMove;
 
 
+    private void Update()
+    {
+        if (redGoal.position.y <= -30) {
+            Vector3 temp = new Vector3(redGoal.position.x, -30, redGoal.position.z);
+            redGoal.position = temp;
+        }
+        if (blueGoal.position.y <= -30)
+        {
+            Vector3 temp = new Vector3(blueGoal.position.x, -30, blueGoal.position.z);
+            blueGoal.position = temp;
+        }
+        if (redGoal.position.y >= 60)
+        {
+            Vector3 temp = new Vector3(redGoal.position.x, 60, redGoal.position.z);
+            redGoal.position = temp;
+        }
+        if (blueGoal.position.y >= 60)
+        {
+            Vector3 temp = new Vector3(blueGoal.position.x, 60, blueGoal.position.z);
+            blueGoal.position = temp;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (this.tag == "goalPost") {
